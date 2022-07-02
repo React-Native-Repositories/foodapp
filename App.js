de96b30287cbfc108701c6711f40ebff15e252f5
kanamui <chrlzmndz.exe@gmail.com>
2022-07-02 15:16:16 +0800

    fix: globally defined theme color

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { NativeBaseProvider, extendTheme, theme as nbTheme } from 'native-base';
+import { NativeBaseProvider, extendTheme } from 'native-base';
 import AppNavigator from './src/navigation/AppNavigator';
 
 const theme = extendTheme({
   colors: {
-    primary: nbTheme.colors.rose,
-  }
+    primary: {
+      50: '#FDF2F7', // bg
+      100: '#FCE7F3',
+      200: '#FBCFE8',
+      300: '#ED66A0', // location pin
+      400: '#FF2B85', // splash
+      500: '#EC4899',
+      600: '#D70F64', // general
+      700: '#BE185D',
+      800: '#9D174D',
+      900: '#831843',
+    },
+    blueGray: {
+      100: '#eff2f4', // bg
+    },
+  },
 });
 
 const config = {
